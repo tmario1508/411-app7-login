f80686089f0de8dc6a9903e4c35892f11fe75c1b
Mario <tmario1508@gmail.com>
2021-10-22 19:19:42 -0500

    Update onet

---
+++ app7-login/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  app7-login
+//
+//  Created by MTWDM301-2021 on 22/10/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    var plist = UserDefaults.standard
+    
+    
+    @IBOutlet weak var txtUsuario: UITextField!
+    
+    @IBOutlet weak var txtPass: UITextField!
+    
+    
+    @IBAction func OnLogin(_ sender: Any) {
+        let usuario = txtUsuario.text
+        let pass = txtPass.text
+        
+        if usuario == "mario" && pass == "1234" {
+            plist.set(usuario, forKey: "usuario")
+            appDelegate.onLoginSuccess()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
